c03cb51e2239a4a6b37c8fe8d80b1c0483461628
Ravern Koh <ravern@taskade.com>
2020-05-23 17:16:29 +0800

    chore: rework migrations

--- web/db/migrations/20200517144835_create-pages.js
+++ web/db/migrations/20200517144835_create-pages.js
@@ -1,7 +1,8 @@
 exports.up = (knex) => {
   return knex.schema.createTable("pages", (t) => {
     t.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
-    t.text("title").unique().notNullable();
+    t.text("title").notNullable();
+    t.json("content").notNullable();
     t.uuid("garden_id").references("gardens.id").notNullable();
   });
 };
